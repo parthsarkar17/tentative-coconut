65ca1893af0cdba89dea4fdcf4515a587462d416
Parth Sarkar <ps679@cornell.edu>
2025-02-08 13:29:51 -0500

    implemented reaching analysis; need to test

--- lesson4/lib/dataflow_analysis.ml
+++ lesson4/lib/dataflow_analysis.ml
@@ -1,19 +1,43 @@
 module type DF_ANALYSIS_TEMPLATE = sig
   type t
+  (** An abstract type to represent a member of the lattice (e.g. could be a set
+      in the powerset lattice.) *)
 
   val empty : t
+  (** The top value in a lattice. *)
+
   val init : Bril.Func.t -> t
-  val transfer : (int * Bril.Instr.t) list -> t -> t
+  (** [init func] is a lattice member that forms as a result of the arguments of
+      function [func]. *)
+
+  val equal : t -> t -> bool
+
+  val transfer : (int * int * Bril.Instr.t) list -> t -> t
+  (** [transfer block mem] is a new member of the lattice that's a result of the
+      contents of the basic block [block] applied to the input lattice member
+      [mem]. Because we might need to identify specific instructions, a basic
+      block here is more than just a list of instructions; the first index of
+      each member of [block] refers to the basic block, and the second index
+      refers to the instruction within the block. *)
+
   val merge : t -> t -> t
+  (** The abstract definition for the meet operator over two members of the
+      lattice. For example, could be the lowest upper bound of two sets in a
+      powerset lattice (i.e. set union). *)
 end
 
+(** A data type for the elements of each lattice member in the powerset lattice
+    where each set represents the reaching definitions for a basic block. *)
 module Definition = struct
-  type t = int * string
+  type t = int * int * string
 
-  let compare (a1, b1) (a2, b2) =
-    match Int.compare a1 a2 with 0 -> String.compare b1 b2 | n -> n
+  let compare (b1, i1, d1) (b2, i2, d2) =
+    match Int.compare b1 b2 with
+    | 0 -> ( match Int.compare i1 i2 with 0 -> String.compare d1 d2 | m -> m)
+    | n -> n
 end
 
+(** Fill out the data flow analysis form for reaching analysis. *)
 module ReachingAnalysis : DF_ANALYSIS_TEMPLATE = struct
   module DefinitionSet = Set.Make (Definition)
 
@@ -25,41 +49,46 @@ module ReachingAnalysis : DF_ANALYSIS_TEMPLATE = struct
     func.args
     |> List.fold_left
          (fun (acc, i) (dest_name, _) ->
-           (DefinitionSet.add (i, dest_name) acc, i + 1))
+           (DefinitionSet.add (0, i, dest_name) acc, i + 1))
          (DefinitionSet.empty, 0)
     |> fst
 
-  let transfer (block : (int * Bril.Instr.t) list) (in_b : t) : t =
-    (* get the variables defined in this block*)
+  let equal = DefinitionSet.equal
+
+  let transfer (block : (int * int * Bril.Instr.t) list) (in_b : t) : t =
     block
+    (* get the definitions defined in this block *)
     |> List.fold_left
-         (fun acc (i, instr) ->
+         (fun acc (b, i, instr) ->
            match Bril.Instr.dest instr with
            | None -> acc
-           | Some (dest_name, _) -> DefinitionSet.add (i, dest_name) acc)
+           | Some (dest_name, _) -> DefinitionSet.add (b, i, dest_name) acc)
          DefinitionSet.empty
     (* unite them with the definitions remaining after removing those killed in this block *)
     |> DefinitionSet.union
          (List.fold_left
-            (fun acc (_, instr) ->
+            (fun acc (_, _, instr) ->
               match Bril.Instr.dest instr with
               | None -> acc
               | Some (instr_dest_name, _) ->
                   DefinitionSet.filter
-                    (fun (_, elt_dest_name) -> elt_dest_name != instr_dest_name)
+                    (fun (_, _, elt_dest_name) ->
+                      elt_dest_name != instr_dest_name)
                     acc)
             in_b block)
 
   let merge = DefinitionSet.union
 end
 
-module DataFlowAnalysis (Template : DF_ANALYSIS_TEMPLATE) = struct
+module DataFlowAnalysis (Lattice : DF_ANALYSIS_TEMPLATE) = struct
+  (* define map modules for storing cfg representation, etc. *)
   module IntValuedMap = Map.Make (Int)
   module StringValuedMap = Map.Make (String)
 
   (** [form_blocks f] is a map from indices of basic blocks to the list of
       instructions forming the block, formed from the function [f]. *)
-  let form_blocks (func : Bril.Func.t) : Bril.Instr.t list IntValuedMap.t =
+  let form_blocks (func : Bril.Func.t) :
+      (int * Bril.Instr.t) list IntValuedMap.t =
     let final_block, other_blocks =
       func |> Bril.Func.instrs
       |> List.fold_left
@@ -76,8 +105,13 @@ module DataFlowAnalysis (Template : DF_ANALYSIS_TEMPLATE) = struct
     |> List.filter (function [] -> false | _ -> true)
     |> List.rev
     |> List.fold_left
-         (fun (i, acc) block -> (i + 1, IntValuedMap.add i block acc))
-         (0, IntValuedMap.empty)
+         (fun (i, acc) block ->
+           ( i + 1,
+             (* map this block's index to an enumerated list of its instructions *)
+             IntValuedMap.add i
+               (List.mapi (fun instr_index instr -> (instr_index, instr)) block)
+               acc ))
+         (1, IntValuedMap.empty)
     |> snd
 
   (** [construct_cfg f] is a pair of maps. The first maps every basic block in
@@ -85,12 +119,15 @@ module DataFlowAnalysis (Template : DF_ANALYSIS_TEMPLATE) = struct
       every block to the indices of its successors. *)
   let construct_cfg (func : Bril.Func.t) :
       int list IntValuedMap.t * int list IntValuedMap.t =
-    (* map index of the basic block to the block's final instruction and the block itself*)
+    (* map index of the basic block to the block's final instruction and the block's instructions *)
     let enumerated_blocks : (int * Bril.Instr.t * Bril.Instr.t list) list =
       func |> form_blocks |> IntValuedMap.to_list
-      |> List.map (fun (i, block) -> (i, block |> List.rev |> List.hd, block))
+      |> List.map (fun (block_idx, enumerated_instrs) ->
+             let instrs = List.map snd enumerated_instrs in
+             (block_idx, instrs |> List.rev |> List.hd, instrs))
     in
-    (* map the label of block to its index and vice versa *)
+    (* map the label of block to its index, and get the first index 
+        that doesn't belong to any basic block *)
     let label_to_id_map, max_id =
       List.fold_left
         (fun (label_to_id, max_id) (id, _, block) ->
@@ -98,7 +135,7 @@ module DataFlowAnalysis (Template : DF_ANALYSIS_TEMPLATE) = struct
             | Bril.Instr.Label lbl -> StringValuedMap.add lbl id label_to_id
             | _ -> label_to_id),
             max_id + 1 ))
-        (StringValuedMap.empty, 0) enumerated_blocks
+        (StringValuedMap.empty, 1) enumerated_blocks
     in
     (* produce a new map with [addition] appended to the list bound to [key] in [map] *)
     let update_map_binding_list key addition map =
@@ -138,13 +175,60 @@ module DataFlowAnalysis (Template : DF_ANALYSIS_TEMPLATE) = struct
       (IntValuedMap.empty, IntValuedMap.empty)
       enumerated_blocks
 
-  let worklist (func : Bril.Func.t) =
+  let worklist (func : Bril.Func.t) :
+      Lattice.t IntValuedMap.t * Lattice.t IntValuedMap.t =
+    (* map from the index of each basic block to a list of enumerated instructions *)
     let basic_blocks = form_blocks func in
+    (* maps from the index of each basic block to a list of indices of its predecessors and successors, respectively *)
     let preds, succs = construct_cfg func in
 
     let rec worklist_aux in_map out_map = function
       | [] -> (in_map, out_map)
-      | block :: worklist -> (in_map, out_map)
+      | (block_idx, block) :: worklist ->
+          let in_block =
+            List.fold_left
+              (fun acc pred ->
+                Lattice.merge acc
+                  (match IntValuedMap.find_opt pred out_map with
+                  | None -> Lattice.empty
+                  | Some set -> set))
+              Lattice.empty
+              (IntValuedMap.find block_idx preds)
+          in
+          let out_block =
+            Lattice.transfer
+              (List.map
+                 (fun (instr_idx, instr) -> (block_idx, instr_idx, instr))
+                 block)
+              in_block
+          in
+          (* if there's a change in this block's output lattice member, 
+              then add its successors to the worklist*)
+          (if
+             match IntValuedMap.find_opt block_idx out_map with
+             | None -> true
+             | Some existing_out_block ->
+                 existing_out_block |> Lattice.equal out_block |> not
+           then
+             List.fold_left
+               (fun acc succ_idx ->
+                 (succ_idx, IntValuedMap.find succ_idx basic_blocks) :: acc)
+               []
+               (IntValuedMap.find block_idx succs)
+             @ worklist
+           else worklist)
+          |> worklist_aux
+               (* replace the lattice member at the input of this block *)
+               (IntValuedMap.update block_idx
+                  (function _ -> Some in_block)
+                  in_map)
+               (* replace the lattice member at the output of this block *)
+               (IntValuedMap.update block_idx
+                  (function _ -> Some out_block)
+                  out_map)
     in
-    ()
+    worklist_aux
+      (IntValuedMap.singleton 0 (Lattice.init func))
+      IntValuedMap.empty
+      (IntValuedMap.to_list basic_blocks)
 end
