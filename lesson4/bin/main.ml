850ada2fffb66c1722057610a9f2269ef9b33ed3
Parth Sarkar <ps679@cornell.edu>
2025-02-13 22:52:30 -0500

    made the output and input flows more readable

--- lesson4/bin/main.ml
+++ lesson4/bin/main.ml
@@ -8,20 +8,26 @@ module ReachingAnalysis =
 let _test_reaching_def () =
   In_channel.stdin |> In_channel.input_all |> Yojson.Basic.from_string
   |> Bril.from_json
-  |> List.map (fun func -> (func, ReachingAnalysis.worklist_algorithm func))
-  |> List.iter (fun ((func, (in_block_map, _)) : Bril.Func.t * _) ->
+  |> List.map (fun func ->
+         (func, Reaching_analysis.consolidate_reaching_defs func))
+  |> List.iter (fun ((func, (in_block_map, out_block_map)) : Bril.Func.t * _) ->
          print_endline ("func: " ^ func.name);
-         in_block_map |> ReachingAnalysis.IntValuedMap.to_seq
-         |> Seq.iter (fun (block_idx, reaching_defs) ->
-                print_endline
-                  (string_of_int block_idx ^ ": "
-                  ^ Reaching_analysis.ReachingAnalysisTemplate.to_string
-                      reaching_defs)));
+         let print_block_map block_map =
+           block_map |> ReachingAnalysis.IntValuedMap.to_seq
+           |> Seq.iter (fun (block_idx, reaching_defs) ->
+                  print_endline
+                    (string_of_int block_idx ^ ": "
+                    ^ String.concat ", " reaching_defs))
+         in
+         print_endline "in: ";
+         print_block_map in_block_map;
+         print_endline "out: ";
+         print_block_map out_block_map);
   print_endline ""
 
 (** For a given Bril program, for each basic block, print out a set of variables
     that can provably only take a single, constant value.*)
-let test_constant_propagation () =
+let _test_constant_propagation () =
   In_channel.stdin |> In_channel.input_all |> Yojson.Basic.from_string
   |> Bril.from_json
   |> List.iter (fun (func : Bril.Func.t) ->
@@ -33,4 +39,4 @@ let test_constant_propagation () =
                   ^ Constant_propagation.ConstantPropagationLattice.to_string
                       single_valued_consts)))
 
-let () = test_constant_propagation ()
+let () = _test_reaching_def ()
--- lesson4/lib/reaching_analysis.ml
+++ lesson4/lib/reaching_analysis.ml
@@ -2,6 +2,8 @@
     where each set represents the reaching definitions for a basic block. *)
 module Definition = struct
   type t = int * int * string
+  (** [t] is the abstract type for a "definition" to perform our reaching
+      definition analysis.*)
 
   let to_string : t -> string = function
     | b, i, dest ->
@@ -14,7 +16,7 @@ module Definition = struct
 end
 
 (** Fill out the data flow analysis form for reaching analysis. *)
-module ReachingAnalysisTemplate : Dataflow_analysis.LATTICE = struct
+module ReachingAnalysisTemplate = struct
   module DefinitionSet = Set.Make (Definition)
 
   type t = DefinitionSet.t
@@ -60,3 +62,37 @@ module ReachingAnalysisTemplate : Dataflow_analysis.LATTICE = struct
     |> List.map (fun mem -> Definition.to_string mem)
     |> List.fold_left (fun acc str_of_mem -> acc ^ str_of_mem ^ "  ") ""
 end
+
+module ReachingAnalysis =
+  Dataflow_analysis.DataFlowAnalysis (ReachingAnalysisTemplate)
+
+module StringSet = Set.Make (String)
+
+let consolidate_reaching_defs func =
+  (* out block contains a dummy set of out flow definitions out of the dummy first block;
+   get rid of it for pretty printing *)
+  let in_blocks, out_blocks =
+    func |> ReachingAnalysis.worklist_algorithm |> fun (x, y) ->
+    (x, ReachingAnalysis.IntValuedMap.remove 0 y)
+  in
+  (* get rid of dummy definitions (i.e. definitions "defined" in block 0, which actually
+  is itself a dummy block)*)
+  let remove_dummy_defs =
+    ReachingAnalysis.IntValuedMap.map
+      (ReachingAnalysisTemplate.DefinitionSet.filter (function
+        | 0, _, _ -> false
+        | _ -> true))
+  in
+  (* since each instruction in a function is distinctly indexed, we need to get rid
+  of the indices and merge all the definitions that share the same destination variable together *)
+  let remove_duplicate_defs =
+    ReachingAnalysis.IntValuedMap.map (fun set ->
+        ReachingAnalysisTemplate.DefinitionSet.fold
+          (fun (_, _, var) acc -> StringSet.add var acc)
+          set StringSet.empty)
+  in
+  let compose block_map =
+    block_map |> remove_dummy_defs |> remove_duplicate_defs
+    |> ReachingAnalysis.IntValuedMap.map StringSet.to_list
+  in
+  (compose in_blocks, compose out_blocks)
