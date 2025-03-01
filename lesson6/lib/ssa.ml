6ede6b48f2b18e921076f0fc0e0ab979e170e569
Parth Sarkar <ps679@cornell.edu>
2025-03-01 12:03:35 -0500

    transformed each basic block to use same variables defined in phi nodes

--- lesson6/lib/ssa.ml
+++ lesson6/lib/ssa.ml
@@ -9,10 +9,10 @@ end
 
 module PairValuedMap = Map.Make (Pair)
 
-(** [unique_identifiers_of_func f] is a map taking a variable-block pair to a
-    unique variable used to represent the variable at the phi node at the
-    beginning of the block; naive implementation of SSA. *)
-let unique_identifiers_of_func (func : Bril.Func.t) : string PairValuedMap.t =
+(** [phi_nodes_of f] is a map taking a variable-block pair to a unique variable
+    used to represent the variable at the phi node at the beginning of the
+    block; naive implementation of SSA. *)
+let phi_nodes_of (func : Bril.Func.t) : string PairValuedMap.t =
   let func_vars : StringSet.t =
     func |> Bril.Func.instrs
     |> List.fold_left
@@ -28,8 +28,66 @@ let unique_identifiers_of_func (func : Bril.Func.t) : string PairValuedMap.t =
          pair2unique
          |> StringSet.fold
               (fun (var : string) ->
-                PairValuedMap.add (var, block_id) (var ^ string_of_int block_id))
+                PairValuedMap.add (var, block_id)
+                  (var ^ "." ^ string_of_int block_id ^ ".1"))
               func_vars
          |> fun x -> (x, block_id + 1))
        (PairValuedMap.empty, 1)
   |> fst
+
+(** [transform_block id b p2u] is a tuple. The first element is a basic block
+    constructed from [b], transformed to use the variable rewrites in [p2u]
+    (which is a total map from original variables to new variables) and that
+    assigns new variables for each local variable overwrite. The second elmement
+    is a total map from each original variable to the newest local variable
+    created to represents that variable. Will be used to insert `set` nodes. *)
+let transform_block (block_id : int) (pair2unique : string PairValuedMap.t)
+    (block : (int * Bril.Instr.t) list) :
+    Bril.Instr.t list * string StringValuedMap.t =
+  block
+  |> List.fold_left
+       (fun ((rev_transformed_block, ogvar2newestvar) :
+              Bril.Instr.t list * string StringValuedMap.t)
+            ((instr_id, instr) : int * Bril.Instr.t) ->
+         let instr_args_transformed : Bril.Instr.t =
+           match Bril.Instr.args instr with
+           | [] -> instr
+           | lst -> (
+               try
+                 Bril.Instr.set_args
+                   (List.map
+                      (fun arg -> StringValuedMap.find arg ogvar2newestvar)
+                      lst)
+                   instr
+               with _ -> instr)
+         in
+         match Bril.Instr.dest instr_args_transformed with
+         | None ->
+             (instr_args_transformed :: rev_transformed_block, ogvar2newestvar)
+         | Some (dest_var, dest_type) ->
+             let new_dest_var =
+               ogvar2newestvar
+               |> StringValuedMap.find dest_var
+               |> String.to_seq |> List.of_seq |> List.rev |> List.tl
+               |> List.rev
+               |> List.map (String.make 1)
+               |> String.concat ""
+               |> ( ^ ) (string_of_int instr_id)
+             in
+             ( Bril.Instr.set_dest
+                 (Some (new_dest_var, dest_type))
+                 instr_args_transformed
+               :: rev_transformed_block,
+               StringValuedMap.update dest_var
+                 (function _ -> Some new_dest_var)
+                 ogvar2newestvar ))
+       ( [],
+         (* construct total mapping from og variable to newest local representation of
+              variable. *)
+         PairValuedMap.fold
+           (fun ((og_var, block_id') : Pair.t) (new_var : string)
+                (acc : string StringValuedMap.t) ->
+             if block_id = block_id' then StringValuedMap.add og_var new_var acc
+             else acc)
+           pair2unique StringValuedMap.empty )
+  |> fun (rev_transformed_block, y) -> (List.rev rev_transformed_block, y)
