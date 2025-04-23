6958a78497040c2035d065608ef63512a66800b5
Parth Sarkar <ps679@cornell.edu>
2025-04-22 23:17:31 -0400

    modified ref interpreter and main.ml to emit/accept specific trace format

--- lesson12/bin/main.ml
+++ lesson12/bin/main.ml
@@ -1,15 +1,25 @@
 let get_trace (interpreter_binary_path : string) (program_path : string)
-    (program_args : string) : unit =
-  (* construct command to run modified reference interpreter *)
-  let json_transformer = "/Users/parthsarkar/.local/bin/bril2json < " in
-  json_transformer ^ program_path ^ " | " ^ interpreter_binary_path ^ " "
-  ^ program_args
-  (* expect the reference interpreter to only log json, string format of its trace *)
-  |> Unix.open_process_in
-  |> In_channel.input_line |> Option.get
-  (* for now, just print out this representation *)
-  |> print_endline
+    (program_args : string) (print_trace : bool) : Bril.Instr.t list =
+  (* construct command to get trace from interpreter*)
+  let command =
+    "/Users/parthsarkar/.local/bin/bril2json < " ^ program_path ^ " | "
+    ^ interpreter_binary_path ^ " " ^ program_args
+  in
+  match
+    (* interpreter is modified to send list of instructions in json format *)
+    command |> Unix.open_process_in |> In_channel.input_lines
+    |> String.concat "" |> Yojson.Basic.from_string
+    |> fun x ->
+    if print_trace then Yojson.Basic.pretty_to_string x |> print_endline else ();
+    x
+  with
+  (* translate this list of json-formatted instrs to a list of bril instrs *)
+  | `List lst -> List.map Bril.Instr.of_json lst
+  | _ -> failwith "Reference interpreter sends wrong format"
 
 let () =
-  get_trace "/Users/parthsarkar/.deno/bin/brili"
-    "../../bril/benchmarks/core/hanoi.bril" "3"
+  let _ =
+    get_trace "deno run ../../bril/brili.ts"
+      "../../bril/benchmarks/core/loopfact.bril" "8" true
+  in
+  ()
