b500a5fbdc6a874ab78228fe6b3480d876ef0c65
Parth Sarkar <ps679@cornell.edu>
2025-04-23 12:55:44 -0400

    added not of guard if false branch is taken in trace

--- lesson12/bin/main.ml
+++ lesson12/bin/main.ml
@@ -3,7 +3,7 @@ let get_trace (interpreter_binary_path : string) (program_path : string)
   (* construct command to get trace from interpreter*)
   let command =
     "/Users/parthsarkar/.local/bin/bril2json < " ^ program_path ^ " | "
-    ^ interpreter_binary_path ^ " " ^ program_args
+    ^ interpreter_binary_path ^ " -t " ^ program_args
   in
   match
     (* interpreter is modified to send list of instructions in json format *)
@@ -24,11 +24,41 @@ let get_trace (interpreter_binary_path : string) (program_path : string)
       lst
   | _ -> failwith "Reference interpreter sends wrong format"
 
+module StringMap = Map.Make (String)
+
 let filter_trace (terminating_instr : Bril.Instr.t) (guard_label : string)
-    (trace : Bril.Instr.t list) : Bril.Instr.t list =
+    (label_map : Bril.Instr.t StringMap.t) (trace : Bril.Instr.t list) :
+    Bril.Instr.t list =
   let rec filter_trace_aux (acc : Bril.Instr.t list) :
       Bril.Instr.t list -> Bril.Instr.t list = function
     | [] -> acc
+    | fst :: snd :: rest -> (
+        match fst with
+        | Bril.Instr.Jmp _ | Bril.Instr.Label _ ->
+            filter_trace_aux acc (snd :: rest)
+        | Bril.Instr.Br (guard, label1, _) ->
+            if snd = StringMap.find label1 label_map then
+              filter_trace_aux
+                (Bril.Instr.Guard (guard, guard_label) :: acc)
+                (snd :: rest)
+            else
+              let not_guard_var_name = "generated_not_" ^ guard in
+              let not_guard =
+                Bril.Instr.Unary
+                  ( (not_guard_var_name, Bril.Bril_type.BoolType),
+                    Bril.Op.Unary.Not,
+                    guard )
+              in
+              filter_trace_aux
+                (Bril.Instr.Guard (not_guard_var_name, guard_label)
+                :: not_guard :: acc)
+                (snd :: rest)
+              (* filter_trace_aux
+              (Bril.Instr.Guard (guard, guard_label) :: acc)
+              trace_rest *)
+        | _ ->
+            if fst = terminating_instr then acc
+            else filter_trace_aux (fst :: acc) (snd :: rest))
     | trace_head :: trace_rest -> (
         match trace_head with
         | Bril.Instr.Jmp _ | Bril.Instr.Label _ ->
@@ -44,14 +74,14 @@ let filter_trace (terminating_instr : Bril.Instr.t) (guard_label : string)
   trace |> filter_trace_aux [] |> List.rev
 
 let insert_into (program_path : string) (program_args : string)
-    (func : Bril.Func.t) : Bril.Func.t =
+    (label_map : Bril.Instr.t StringMap.t) (func : Bril.Func.t) : Bril.Func.t =
   let trace =
-    get_trace "deno run ../../bril/brili.ts" program_path program_args false
+    get_trace "deno run ../../bril/brili.ts " program_path program_args false
     |> filter_trace
          ("{ \"args\": [ \"result\" ], \"dest\": \"v13\", \"op\": \"id\", \
            \"type\": \"int\" }" |> Yojson.Basic.from_string
         |> Bril.Instr.of_json)
-         "__b0"
+         "__b0" label_map
   in
   let existing_instrs = Bril.Func.instrs func in
   let new_instrs =
@@ -62,17 +92,32 @@ let insert_into (program_path : string) (program_args : string)
   in
   Bril.Func.set_instrs func new_instrs
 
+let build_map (func : Bril.Func.t) : Bril.Instr.t StringMap.t =
+  let rec build_map_aux (instr_list : Bril.Instr.t list)
+      (acc : Bril.Instr.t StringMap.t) : Bril.Instr.t StringMap.t =
+    match instr_list with
+    | [] | [ _ ] -> acc
+    | fst :: snd :: rest -> (
+        match fst with
+        | Bril.Instr.Label label ->
+            build_map_aux (snd :: rest) (StringMap.add label snd acc)
+        | _ -> build_map_aux (snd :: rest) acc)
+  in
+  build_map_aux (Bril.Func.instrs func) StringMap.empty
+
 let run (program_path : string) (program_args : string) : unit =
   let program : Bril.t =
     "/Users/parthsarkar/.local/bin/bril2json < " ^ program_path
     |> Unix.open_process_in |> In_channel.input_all |> Yojson.Basic.from_string
     |> Bril.from_json
   in
-  let _main : Bril.Func.t =
+
+  let main : Bril.Func.t =
     List.find (fun (func : Bril.Func.t) -> func.name = "main") program
   in
+  let label2first_instr = build_map main in
   let main_with_trace : Bril.Func.t =
-    insert_into program_path program_args _main
+    insert_into program_path program_args label2first_instr main
   in
   let program_with_trace =
     main_with_trace
